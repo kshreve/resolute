3a14bdf72fb211ee31fb42d19fb6f84e5ef806dc
Kevin Shreve <kevinshreve@flashrecruit.com>
2017-10-24 08:11:08 -0400

    remove text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
                     <h1 className="App-title">Test</h1>
                 </header>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
                     <Leaderboard/>
                 </p>
             </div>
